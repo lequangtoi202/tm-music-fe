e5c65bd6cdb679cd59dc691dd7e515f46e14396c
vanmai1912 <mai.dv01@gmail.com>
2024-05-11 03:59:35 +0700

    update

--- src/components/TextHeader/index.tsx
+++ src/components/TextHeader/index.tsx
@@ -2,13 +2,17 @@ import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import Text from '../Text';
 import { Container, StyledTextHeader, StyledViewMore } from './styles';
 import { TextHeaderProps } from './types';
+import { Link } from 'react-router-dom';
 
 const TextHeader: React.FC<TextHeaderProps> = ({ text }) => {
   return (
     <Container>
       <StyledTextHeader>{text}</StyledTextHeader>
       <StyledViewMore>
-        <Text>{'TẤT CẢ'}</Text>
+        <Link to='/chu-de'>
+          <Text>{'TẤT CẢ'}</Text>
+        </Link>
+        
         <KeyboardArrowRightIcon sx={{ fontSize: 32 }} />
       </StyledViewMore>
     </Container>
